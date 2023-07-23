d94f029e18416d026c6b89f8fe647c6098a4d076
YlStar1977 <128179443+YlStar1977@users.noreply.github.com>
2023-07-23 20:45:56 +0300

    02-js, 01-js end

---
+++ js/01-gallery.js
@@ -0,0 +1,60 @@
+import { galleryItems } from './gallery-items.js';
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>
+        `;
+    }).join('');
+}
+
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryItemsMarkup(galleryItems));
+
+galleryList.addEventListener('click', onGalleryItemClick);
+
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+    
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+ 
+    const largeImageUrl = event.target.dataset.source;
+
+  
+    openModal(largeImageUrl);
+}
+
+function openModal(url) {
+    const instance = basicLightbox.create(`
+        <img src="${url}" alt="Image description" />
+    `);
+
+    instance.show();
+
+    document.addEventListener('keydown', onModalKeyPress);
+
+  
+    function onModalKeyPress(event) {
+        if (event.key === 'Escape') {
+            instance.close();
+            document.removeEventListener('keydown', onModalKeyPress);
+        }
+    }
+}
+
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,33 @@
+
+import { galleryItems } from './gallery-items.js';
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+    `;
+    })
+    .join('');
+}
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryItemsMarkup(galleryItems));
+
+document.addEventListener('DOMContentLoaded', function () {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: 'true',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    
+  });
+});
